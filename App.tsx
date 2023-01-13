f13f6fdf92f15c7dfbb9154b8eb6d69b68f026b3
Milo Bedini <milobedini64@gmail.com>
2023-01-13 21:22:16 +0000

    added temporary registration bypass

--- App.tsx
+++ App.tsx
@@ -1,90 +1,90 @@
-import "react-native-gesture-handler";
-import { LinkingOptions, NavigationContainer } from "@react-navigation/native";
-import * as Linking from "expo-linking";
-import { useEffect, useState } from "react";
-import { StatusBar } from "react-native";
-import { FontsContainer } from "./components/fonts-container";
-import { AppNavigator, AppNavigatorParamList } from "./navigators/app";
+import 'react-native-gesture-handler'
+import { LinkingOptions, NavigationContainer } from '@react-navigation/native'
+import * as Linking from 'expo-linking'
+import { useEffect, useState } from 'react'
+import { StatusBar } from 'react-native'
+import { FontsContainer } from './components/fonts-container'
+import { AppNavigator, AppNavigatorParamList } from './navigators/app'
 
-const prefix = Linking.createURL("/");
+const prefix = Linking.createURL('/')
 
-export type AppProps = LaunchArguments;
+export type AppProps = LaunchArguments
 
 const App = (props: AppProps): JSX.Element => {
-  const [init, setInit] = useState(true);
+  const [init, setInit] = useState(true)
 
   useEffect(() => {
-    if (!init) return;
+    if (!init) return
 
     if (props.reset) {
-      console.log("Resetting");
+      console.log('Resetting')
       // clear all settings
     }
-    setInit(false);
-  }, [init]);
+    setInit(false)
+  }, [init])
 
   const linking: LinkingOptions<AppNavigatorParamList> = {
     prefixes: [prefix],
     config: {
       screens: {
         app: {
-          path: "app",
+          path: 'app',
           screens: {
             welcome: {
-              path: "welcome",
+              path: 'welcome',
               // screens: {
               //   signInMagic: {
               //     path: 'sign-in-magic'
               //   }
               // }
             },
-            // main: {
-            //   screens: {
-            //     home: {
-            //       screens: {
-            //         homepage: {
-            //           path: 'home'
-            //         }
-            //       }
-            //     },
-            //     planner: {
-            //       path: 'plan',
-            //       screens: {
-            //         plan: ':date',
-            //         addRecipe: {
-            //           path: ':date/add-recipe'
-            //         }
-            //       }
-            //     },
-            //     recipes: {
-            //       screens: {
-            //         show: {
-            //           path: 'recipes/:recipeId'
-            //         },
-            //         list: {
-            //           path: 'recipes'
-            //         }
-            //       }
-            //     },
-            //     'shopping-list': 'shopping-list',
-            //     'more-info': {
-            //       path: 'more-info',
-            //       screens: {
-            //         index: '',
-            //         'delete-account': 'delete-account'
-            //       }
-            //     }
-            //   }
-            // }
+            main: {
+              screens: {
+                home: {
+                  screens: {
+                    homepage: {
+                      path: 'home',
+                    },
+                  },
+                },
+                // planner: {
+                //   path: 'plan',
+                //   screens: {
+                //     plan: ':date',
+                //     addRecipe: {
+                //       path: ':date/add-recipe'
+                //     }
+                //   }
+                // },
+                // recipes: {
+                //   screens: {
+                //     show: {
+                //       path: 'recipes/:recipeId'
+                //     },
+                //     list: {
+                //       path: 'recipes'
+                //     }
+                //   }
+                // },
+                // 'shopping-list': 'shopping-list',
+                // 'more-info': {
+                //   path: 'more-info',
+                //   screens: {
+                //     index: '',
+                //     'delete-account': 'delete-account'
+                //   }
+                // }
+              },
+            },
           },
         },
       },
     },
-  };
+  }
   return (
     <>
       <StatusBar
-        backgroundColor={"blue"}
+        backgroundColor={'blue'}
         barStyle="light-content"
         translucent
       />
@@ -94,7 +94,7 @@ const App = (props: AppProps): JSX.Element => {
         </FontsContainer>
       </NavigationContainer>
     </>
-  );
-};
+  )
+}
 
-export default App;
+export default App
--- navigators/home/screens/homepage/index.tsx
+++ navigators/home/screens/homepage/index.tsx
@@ -1,47 +1,48 @@
-import { StackScreenProps } from "@react-navigation/stack";
+import { StackScreenProps } from '@react-navigation/stack'
 import {
   Image,
   ImageBackground,
   StyleSheet,
   Text,
   TouchableOpacity,
+  useWindowDimensions,
   View,
-} from "react-native";
-import { Colours } from "../../../../colours";
-import { textStyles } from "../../../../components/text";
-import { HomeNavigatorParamsList } from "../../HomeNavigatorParamsList";
-import meditateImage from "./med.png";
-import communityImage from "./book.png";
-import notesImage from "./cloud.png";
-import timerImage from "./timer.png";
-import progressImage from "./progress.png";
-import backgroundImage from "./background.png";
+} from 'react-native'
+import { Colours } from '../../../../colours'
+import { textStyles } from '../../../../components/text'
+import { HomeNavigatorParamsList } from '../../HomeNavigatorParamsList'
+import meditateImage from './med.png'
+import communityImage from './book.png'
+import notesImage from './cloud.png'
+import timerImage from './timer.png'
+import progressImage from './progress.png'
+import backgroundImage from './background.png'
+import backgroundWeb from './background_web.png'
 export type HomeScreenProps = StackScreenProps<
   HomeNavigatorParamsList,
-  "homepage"
->;
+  'homepage'
+>
 
 const styles = StyleSheet.create({
   content: {
     flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   background: {
-    width: "100%",
-    height: "100%",
-    alignItems: "center",
-    justifyContent: "center",
+    width: '100%',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   row: {
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "center",
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
     padding: 20,
   },
   imageContainer: {
-    alignItems: "center",
-    justifyContent: "center",
+    alignItems: 'center',
+    justifyContent: 'center',
     margin: 10,
     paddingHorizontal: 8,
   },
@@ -51,19 +52,23 @@ const styles = StyleSheet.create({
   },
   imageTitle: {
     marginTop: 10,
-    textAlign: "center",
+    textAlign: 'center',
     color: Colours.lightGrey.$,
   },
-});
+})
 
 export const HomeScreen = ({ navigation }: HomeScreenProps): JSX.Element => {
+  const { width, height } = useWindowDimensions()
   return (
     <View>
-      <ImageBackground style={styles.background} source={backgroundImage}>
+      <ImageBackground
+        style={[styles.background, { height: height }]}
+        source={width > 480 ? backgroundWeb : backgroundImage}
+      >
         <View>
           <View style={styles.content}>
             <View style={styles.row}>
-              <TouchableOpacity onPress={() => console.log("navigating")}>
+              <TouchableOpacity onPress={() => console.log('navigating')}>
                 <View style={styles.imageContainer}>
                   <Image source={meditateImage} style={styles.image} />
                   <Text style={[textStyles.body, styles.imageTitle]}>
@@ -71,7 +76,7 @@ export const HomeScreen = ({ navigation }: HomeScreenProps): JSX.Element => {
                   </Text>
                 </View>
               </TouchableOpacity>
-              <TouchableOpacity onPress={() => console.log("navigating")}>
+              <TouchableOpacity onPress={() => console.log('navigating')}>
                 <View style={styles.imageContainer}>
                   <Image source={communityImage} style={styles.image} />
                   <Text style={[textStyles.body, styles.imageTitle]}>
@@ -81,7 +86,7 @@ export const HomeScreen = ({ navigation }: HomeScreenProps): JSX.Element => {
               </TouchableOpacity>
             </View>
             <View style={styles.row}>
-              <TouchableOpacity onPress={() => console.log("navigating")}>
+              <TouchableOpacity onPress={() => console.log('navigating')}>
                 <View style={styles.imageContainer}>
                   <Image source={notesImage} style={styles.image} />
                   <Text style={[textStyles.body, styles.imageTitle]}>
@@ -89,7 +94,7 @@ export const HomeScreen = ({ navigation }: HomeScreenProps): JSX.Element => {
                   </Text>
                 </View>
               </TouchableOpacity>
-              <TouchableOpacity onPress={() => console.log("navigating")}>
+              <TouchableOpacity onPress={() => console.log('navigating')}>
                 <View style={styles.imageContainer}>
                   <Image source={timerImage} style={styles.image} />
                   <Text style={[textStyles.body, styles.imageTitle]}>
@@ -97,7 +102,7 @@ export const HomeScreen = ({ navigation }: HomeScreenProps): JSX.Element => {
                   </Text>
                 </View>
               </TouchableOpacity>
-              <TouchableOpacity onPress={() => console.log("navigating")}>
+              <TouchableOpacity onPress={() => console.log('navigating')}>
                 <View style={styles.imageContainer}>
                   <Image source={progressImage} style={styles.image} />
                   <Text style={[textStyles.body, styles.imageTitle]}>
@@ -110,5 +115,5 @@ export const HomeScreen = ({ navigation }: HomeScreenProps): JSX.Element => {
         </View>
       </ImageBackground>
     </View>
-  );
-};
+  )
+}
