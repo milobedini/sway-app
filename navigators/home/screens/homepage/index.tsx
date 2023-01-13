498325c7d7a06811130dfc2922938fdb4c0542e6
Milo Bedini <milo.bedini@dae.mn>
2023-01-13 19:10:00 +0000

    feat: created home page [sway-2]

--- navigators/home/screens/homepage/index.tsx
+++ navigators/home/screens/homepage/index.tsx
@@ -1,27 +1,114 @@
-import { StackScreenProps } from '@react-navigation/stack'
-import { StyleSheet, Text, View } from 'react-native'
-import { SafeAreaView } from 'react-native-safe-area-context'
-import { Colours } from '../../../../colours'
-import { textStyles } from '../../../../components/text'
-import { HomeNavigatorParamsList } from '../../HomeNavigatorParamsList'
-
+import { StackScreenProps } from "@react-navigation/stack";
+import {
+  Image,
+  ImageBackground,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import { Colours } from "../../../../colours";
+import { textStyles } from "../../../../components/text";
+import { HomeNavigatorParamsList } from "../../HomeNavigatorParamsList";
+import meditateImage from "./med.png";
+import communityImage from "./book.png";
+import notesImage from "./cloud.png";
+import timerImage from "./timer.png";
+import progressImage from "./progress.png";
+import backgroundImage from "./background.png";
 export type HomeScreenProps = StackScreenProps<
   HomeNavigatorParamsList,
-  'homepage'
->
+  "homepage"
+>;
 
 const styles = StyleSheet.create({
-  container: {
+  content: {
     flex: 1,
-    backgroundColor: Colours.dark.$,
-    alignItems: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
-})
+  background: {
+    width: "100%",
+    height: "100%",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  row: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 20,
+  },
+  imageContainer: {
+    alignItems: "center",
+    justifyContent: "center",
+    margin: 10,
+    paddingHorizontal: 8,
+  },
+  image: {
+    width: 60,
+    height: 60,
+  },
+  imageTitle: {
+    marginTop: 10,
+    textAlign: "center",
+    color: Colours.lightGrey.$,
+  },
+});
 
-export const HomeScreen = () => {
+export const HomeScreen = ({ navigation }: HomeScreenProps): JSX.Element => {
   return (
-    <SafeAreaView style={styles.container}>
-      <Text style={textStyles.title}>Home Screen</Text>
-    </SafeAreaView>
-  )
-}
+    <View>
+      <ImageBackground style={styles.background} source={backgroundImage}>
+        <View>
+          <View style={styles.content}>
+            <View style={styles.row}>
+              <TouchableOpacity onPress={() => console.log("navigating")}>
+                <View style={styles.imageContainer}>
+                  <Image source={meditateImage} style={styles.image} />
+                  <Text style={[textStyles.body, styles.imageTitle]}>
+                    Meditate
+                  </Text>
+                </View>
+              </TouchableOpacity>
+              <TouchableOpacity onPress={() => console.log("navigating")}>
+                <View style={styles.imageContainer}>
+                  <Image source={communityImage} style={styles.image} />
+                  <Text style={[textStyles.body, styles.imageTitle]}>
+                    Community
+                  </Text>
+                </View>
+              </TouchableOpacity>
+            </View>
+            <View style={styles.row}>
+              <TouchableOpacity onPress={() => console.log("navigating")}>
+                <View style={styles.imageContainer}>
+                  <Image source={notesImage} style={styles.image} />
+                  <Text style={[textStyles.body, styles.imageTitle]}>
+                    Notes
+                  </Text>
+                </View>
+              </TouchableOpacity>
+              <TouchableOpacity onPress={() => console.log("navigating")}>
+                <View style={styles.imageContainer}>
+                  <Image source={timerImage} style={styles.image} />
+                  <Text style={[textStyles.body, styles.imageTitle]}>
+                    Timer
+                  </Text>
+                </View>
+              </TouchableOpacity>
+              <TouchableOpacity onPress={() => console.log("navigating")}>
+                <View style={styles.imageContainer}>
+                  <Image source={progressImage} style={styles.image} />
+                  <Text style={[textStyles.body, styles.imageTitle]}>
+                    Profile
+                  </Text>
+                </View>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </View>
+      </ImageBackground>
+    </View>
+  );
+};
