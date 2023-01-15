bf3c5d3a336a4d2ae539a5e3b1202052348c0029
Milo Bedini <milo.bedini@dae.mn>
2023-01-15 14:25:29 +0000

    feat: home navigation button [sway-17]

--- navigators/home/screens/homepage/index.tsx
+++ navigators/home/screens/homepage/index.tsx
@@ -1,116 +1,83 @@
 import { StackScreenProps } from "@react-navigation/stack";
 import {
+  Image,
   ImageBackground,
   StyleSheet,
+  Text,
+  TouchableOpacity,
   useWindowDimensions,
   View,
 } from "react-native";
 
 import { Colours } from "../../../../colours";
+import { textStyles } from "../../../../components/text";
 import { HomeNavigatorParamsList } from "../../HomeNavigatorParamsList";
 import backgroundImage from "./background.png";
 import backgroundWeb from "./background_web.png";
+import meditationImage from "./logo_black.png";
 export type HomeScreenProps = StackScreenProps<
   HomeNavigatorParamsList,
   "homepage"
 >;
 
 const styles = StyleSheet.create({
-  content: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  background: {
-    width: "100%",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  row: {
+  container: { flex: 1 },
+  background: { flex: 0.5, justifyContent: "center", textAlign: "center" },
+  sway: { textAlign: "center" },
+  button: {
     flexDirection: "row",
+    width: 300,
+    alignSelf: "center",
+    justifyContent: "space-evenly",
     alignItems: "center",
-    justifyContent: "center",
-    padding: 20,
-  },
-  imageContainer: {
-    alignItems: "center",
-    justifyContent: "center",
-    margin: 10,
-    paddingHorizontal: 8,
+    marginTop: "10%",
+    backgroundColor: "rgba(12, 21, 39, 0.6)",
+    borderRadius: 8,
+    borderWidth: 1,
+    borderColor: Colours.bright.$,
   },
   image: {
-    width: 60,
-    height: 60,
+    width: 120,
+    height: 120,
   },
   imageTitle: {
-    marginTop: 10,
     textAlign: "center",
+    fontSize: 20,
     color: Colours.lightGrey.$,
+    maxWidth: "50%",
   },
 });
+
+/* 
+WIREFRAME
+
+Should link to the latest/daily meditation only.
+
+*/
 // eslint-disable-next-line
 export const HomeScreen = ({ navigation }: HomeScreenProps): JSX.Element => {
   const { width, height } = useWindowDimensions();
   return (
-    <View>
+    <View style={styles.container}>
       <ImageBackground
         style={[styles.background, { height: height }]}
         source={width > 480 ? backgroundWeb : backgroundImage}
       >
-        <View>
-          <View style={styles.content}>
-            <View style={styles.row}>
-              {/* <TouchableOpacity
-                onPress={() =>
-                  navigation
-                    .getParent()
-                    ?.navigate("meditate", { screen: "list", params: {} })
-                }
-              >
-                <View style={styles.imageContainer}>
-                  <Image source={meditateImage} style={styles.image} />
-                  <Text style={[textStyles.body, styles.imageTitle]}>
-                    Meditate
-                  </Text>
-                </View>
-              </TouchableOpacity>
-              <TouchableOpacity onPress={() => console.log("navigating")}>
-                <View style={styles.imageContainer}>
-                  <Image source={communityImage} style={styles.image} />
-                  <Text style={[textStyles.body, styles.imageTitle]}>
-                    Community
-                  </Text>
-                </View>
-              </TouchableOpacity>
-            </View>
-            <View style={styles.row}>
-              <TouchableOpacity onPress={() => console.log("navigating")}>
-                <View style={styles.imageContainer}>
-                  <Image source={notesImage} style={styles.image} />
-                  <Text style={[textStyles.body, styles.imageTitle]}>
-                    Notes
-                  </Text>
-                </View>
-              </TouchableOpacity>
-              <TouchableOpacity onPress={() => console.log("navigating")}>
-                <View style={styles.imageContainer}>
-                  <Image source={timerImage} style={styles.image} />
-                  <Text style={[textStyles.body, styles.imageTitle]}>
-                    Timer
-                  </Text>
-                </View>
-              </TouchableOpacity>
-              <TouchableOpacity onPress={() => console.log("navigating")}>
-                <View style={styles.imageContainer}>
-                  <Image source={progressImage} style={styles.image} />
-                  <Text style={[textStyles.body, styles.imageTitle]}>
-                    Profile
-                  </Text>
-                </View>
-              </TouchableOpacity> */}
-            </View>
-          </View>
-        </View>
+        <Text style={[textStyles.title, styles.sway]}>Sway</Text>
+        <TouchableOpacity
+          onPress={() =>
+            navigation
+              .getParent()
+              ?.navigate("meditate", { screen: "list", params: {} })
+          }
+          style={[styles.button]}
+          activeOpacity={0.4}
+        >
+          <Text style={[textStyles.title, styles.imageTitle]}>
+            Your Daily Meditation
+          </Text>
+          <Image source={meditationImage} style={styles.image} />
+        </TouchableOpacity>
       </ImageBackground>
     </View>
   );
--- navigators/meditate/screens/meditate-list/MeditateListScreen.tsx
+++ navigators/meditate/screens/meditate-list/MeditateListScreen.tsx
@@ -29,6 +29,13 @@ export const MeditateListScreen = ({
     testApi();
   }, []);
 
+  /* 
+  WIREFRAME
+  Basically want category tiles that expand to show each. 
+  (Effectively a filter in the background).
+  Then clicking on ne takes you to recipe show. This has all the info and a button to start listening.
+  The button then launches a media player module on top of the show screen.
+  */
   return (
     <>
       <StatusBar style="dark" />
