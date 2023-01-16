70852a7733c33c8a11e1be67621654208a06acaa
Milo Bedini <milo.bedini@dae.mn>
2023-01-16 00:55:52 +0000

    added live sway api from elastic beanstalk

--- lib/api/api.tsx
+++ lib/api/api.tsx
@@ -1,1 +1,3 @@
-export const baseUrl = "http://localhost:8000/api";
+// export const baseUrl = "http://localhost:8000/api";
+export const baseUrl =
+  "http://sway-env.eba-mc4svv2c.eu-west-1.elasticbeanstalk.com/api";
