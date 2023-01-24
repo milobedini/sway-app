b3725be46aa662690866d9632e343b97010521b7
Milo Bedini <milo.bedini@dae.mn>
2023-01-24 17:28:41 +0000

    feat: get started animation [sway-41]

---
+++ components/circle-animation/index.tsx
@@ -0,0 +1,135 @@
+import { Animated, StyleSheet, TouchableOpacity } from "react-native";
+
+import { Colours } from "../../colours";
+import logo from "../../assets/logo_black.png";
+
+const CIRCLE_SIZE = 100;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "flex-start",
+    alignItems: "center",
+  },
+  circleContainer: {
+    flex: 1,
+    justifyContent: "flex-end",
+    alignItems: "center",
+    padding: 8,
+    paddingBottom: 100,
+    backgroundColor: Colours.bright.$,
+  },
+  circle: {
+    backgroundColor: Colours.dark.$,
+    width: CIRCLE_SIZE,
+    height: CIRCLE_SIZE,
+    borderRadius: CIRCLE_SIZE / 2,
+  },
+  circleButton: {
+    backgroundColor: "transparent",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  button: {
+    alignSelf: "stretch",
+    marginHorizontal: 50,
+  },
+  image: {
+    // height: 500,
+    // width: 800,
+    // resizeMode: "contain",
+  },
+});
+
+interface CircleProps {
+  onPress: () => void;
+  //   animatedValue: {interpolate : {(inputRange: [], outputRange: []}) => void};
+  animatedValue: Animated.Value;
+}
+
+export const Circle = ({ onPress, animatedValue }: CircleProps) => {
+  const inputRange = [0, 0.001, 0.5, 0.501, 1];
+  const containerBg = animatedValue.interpolate({
+    inputRange,
+    outputRange: [
+      Colours.dark.$,
+      Colours.dark.$,
+      Colours.dark.$,
+      Colours.bright.$,
+      Colours.bright.$,
+    ],
+  });
+  const circleBg = animatedValue.interpolate({
+    inputRange,
+    outputRange: [
+      Colours.bright.$,
+      Colours.bright.$,
+      Colours.bright.$,
+      Colours.dark.$,
+      Colours.dark.$,
+    ],
+  });
+  return (
+    <Animated.View
+      style={[
+        StyleSheet.absoluteFillObject,
+        styles.circleContainer,
+        {
+          backgroundColor: containerBg,
+        },
+      ]}
+    >
+      <Animated.View
+        style={[
+          styles.circle,
+          {
+            transform: [
+              {
+                // Make it look closer to camera.
+                perspective: 400,
+              },
+              {
+                rotateY: animatedValue.interpolate({
+                  inputRange: [0, 0.5, 1],
+                  outputRange: ["0deg", "-90deg", "-180deg"],
+                }),
+              },
+              {
+                scale: animatedValue.interpolate({
+                  inputRange: [0, 0.5, 1],
+                  outputRange: [1, 8, 1],
+                }),
+              },
+              {
+                translateX: animatedValue.interpolate({
+                  inputRange: [0, 0.5, 1],
+                  // outputRange: [0, (width * 0.5) / width, 0],
+                  outputRange: [0, 0.5, 0],
+                }),
+              },
+            ],
+          },
+        ]}
+      >
+        <TouchableOpacity onPress={onPress}>
+          <Animated.View
+            style={[
+              styles.circle,
+              styles.circleButton,
+              { backgroundColor: circleBg },
+            ]}
+          >
+            <Animated.Image
+              source={logo}
+              style={[
+                styles.image,
+                styles.circle,
+                { backgroundColor: circleBg },
+              ]}
+            />
+          </Animated.View>
+        </TouchableOpacity>
+      </Animated.View>
+    </Animated.View>
+  );
+};
--- navigators/welcome/screens/ready/index.tsx
+++ navigators/welcome/screens/ready/index.tsx
@@ -1,13 +1,16 @@
 import { StackScreenProps } from "@react-navigation/stack";
-import { Image, StyleSheet, Text } from "react-native";
+import { Image, StyleSheet, Text, View, Animated } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import * as SplashScreen from "expo-splash-screen";
+import { StatusBar } from "expo-status-bar";
+import { useRef, useState } from "react";
 
 import { Colours } from "../../../../colours";
 import { textStyles } from "../../../../components/text";
 import { WelcomeNavigatorParamsList } from "../../WelcomeNavigatorParamsList";
 import logo from "../../../../assets/logo_black.png";
 import { PrimaryButton } from "../../../../components/primary-button";
+import { Circle } from "../../../../components/circle-animation";
 
 export type ReadyScreenProps = StackScreenProps<
   WelcomeNavigatorParamsList,
@@ -37,22 +40,50 @@ const styles = StyleSheet.create({
 });
 
 export const ReadyScreen = ({ navigation }: ReadyScreenProps): JSX.Element => {
+  const [started, setStarted] = useState(false);
   const onGetStarted = () => {
-    navigation.push("breathe"), [navigation];
+    setStarted(true);
+    onPress();
+    setTimeout(() => {
+      navigation.push("breathe"), [navigation];
+    }, 3000);
   };
+
+  const animatedValue = useRef(new Animated.Value(0)).current;
+  const animation = (toValue: number) =>
+    Animated.timing(animatedValue, {
+      toValue,
+      duration: 3000,
+      useNativeDriver: false,
+    });
+
+  const [index, setIndex] = useState(0);
+  const onPress = () => {
+    setIndex(index === 1 ? 0 : 1);
+    animation(index === 1 ? 0 : 1).start();
+  };
+  if (!started) {
+    return (
+      <SafeAreaView style={styles.container}>
+        <Image
+          source={logo}
+          style={styles.image}
+          onLoad={() => SplashScreen.hideAsync()}
+        />
+        <Text style={[textStyles.title, styles.title]}>Welcome to Sway</Text>
+        <PrimaryButton
+          title="Get Started!"
+          onPress={onGetStarted}
+          style={styles.button}
+        />
+        {/* <Circle onPress={onPress} animatedValue={animatedValue} /> */}
+      </SafeAreaView>
+    );
+  }
   return (
-    <SafeAreaView style={styles.container}>
-      <Image
-        source={logo}
-        style={styles.image}
-        onLoad={() => SplashScreen.hideAsync()}
-      />
-      <Text style={[textStyles.title, styles.title]}>Welcome to Sway</Text>
-      <PrimaryButton
-        title="Get Started!"
-        onPress={onGetStarted}
-        style={styles.button}
-      />
-    </SafeAreaView>
+    <View style={styles.container}>
+      <StatusBar style="auto" hidden />
+      <Circle onPress={onPress} animatedValue={animatedValue} />
+    </View>
   );
 };
