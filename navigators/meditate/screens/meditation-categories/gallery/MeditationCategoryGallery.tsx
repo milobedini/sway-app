23570425da4cb05a3d5b729c0e2eb5b63b7b08dc
milobedini <milobedini64@gmail.com>
2023-01-27 19:54:40 +0000

    feat: meditation category filtering [sway-48]

---
+++ navigators/meditate/screens/meditation-categories/gallery/MeditationCategoryGallery.tsx
@@ -0,0 +1,19 @@
+import image1 from "./one.png";
+import image2 from "./two.png";
+import image3 from "./three.png";
+import image4 from "./four.png";
+import image5 from "./five.png";
+import image6 from "./six.png";
+import image7 from "./seven.png";
+import image8 from "./eight.png";
+
+export const meditationCategoryGallery = [
+  image1,
+  image2,
+  image3,
+  image4,
+  image5,
+  image6,
+  image7,
+  image8,
+];
