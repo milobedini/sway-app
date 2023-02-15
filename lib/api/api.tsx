8d2efe105068e7d76ffa3b3e505659874099b2a0
milobedini <milobedini64@gmail.com>
2023-02-15 16:22:18 +0000

    feat: about page [sway-71]

--- lib/api/api.tsx
+++ lib/api/api.tsx
@@ -1,8 +1,8 @@
 import { getToken } from "../auth/auth";
 
-export const baseUrl = "http://localhost:8000/api";
-// export const baseUrl =
-//   "http://sway-env.eba-mc4svv2c.eu-west-1.elasticbeanstalk.com/api";
+// export const baseUrl = "http://localhost:8000/api";
+export const baseUrl =
+  "http://sway-env.eba-mc4svv2c.eu-west-1.elasticbeanstalk.com/api";
 
 //   switch when developing
 
