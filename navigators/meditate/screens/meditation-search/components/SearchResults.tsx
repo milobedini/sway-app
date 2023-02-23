546f9475869bbef0179499e8508c543e71d170fa
milobedini <milobedini64@gmail.com>
2023-02-23 09:46:33 +0000

    feat: meditation search [sway-72]

--- navigators/meditate/screens/meditation-search/components/SearchResults.tsx
+++ navigators/meditate/screens/meditation-search/components/SearchResults.tsx
@@ -1,22 +1,38 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { FlatList, Text } from "react-native";
+import {
+  FlatList,
+  ImageSourcePropType,
+  Text,
+  TouchableOpacity,
+} from "react-native";
 
-import { MeditationListResponseDataItem } from "../../../../../components/meditate-list/mapMeditationTileData";
+import {
+  mapMeditationTileData,
+  MeditationListResponseDataItem,
+} from "../../../../../components/meditate-list/mapMeditationTileData";
+import { MeditationTile } from "../../../../../components/meditation-tile";
 import { designStyles } from "../../../../../components/text";
 import { baseUrl } from "../../../../../lib/api/api";
+import { shuffle } from "../../../../../lib/shuffle";
+import { ThenThrow } from "../../../../../lib/then-throw";
+import { meditationGallery } from "../../meditation-menu/gallery/MeditationGallery";
 
 type SearchResultsProps = {
   text: string;
   searched: boolean;
+  onPress: (meditationId: number, image: ImageSourcePropType) => void;
 };
 type SearchResultDataItem = {
   item: MeditationListResponseDataItem;
   index: number;
 };
+const images = meditationGallery;
+const randomisedImages = shuffle([...images]);
 export const SearchResults = ({
   text,
   searched,
+  onPress,
 }: SearchResultsProps): JSX.Element => {
   const [results, setResults] = useState([]);
 
@@ -34,9 +50,23 @@ export const SearchResults = ({
         data={results}
         renderItem={({ item, index }: SearchResultDataItem) => {
           return (
-            <Text key={index} style={[designStyles.body]}>
-              {item.name}
-            </Text>
+            <TouchableOpacity
+              onPress={() =>
+                onPress(
+                  Number(item.id),
+                  //@ts-expect-error image type
+                  randomisedImages[index % randomisedImages.length] ??
+                    ThenThrow("Missing meditation id!")
+                )
+              }
+              style={{ marginTop: 16 }}
+            >
+              <MeditationTile
+                key={item.id}
+                meditation={mapMeditationTileData(item)}
+                image={randomisedImages[index % randomisedImages.length]}
+              />
+            </TouchableOpacity>
           );
         }}
       >
@@ -44,5 +74,9 @@ export const SearchResults = ({
       </FlatList>
     );
   }
-  return <Text style={[designStyles.body]}>No Results</Text>;
+  return (
+    <Text style={[designStyles.body, { marginTop: 16 }]}>
+      No results match {text}
+    </Text>
+  );
 };
